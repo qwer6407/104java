43c0d2d937438d4eca423ca6dea046538bab24cf
qwer6407 <104021021@live.asia.com.tw>
2015-08-03 16:29:39 +0800

    JAVA SO different

---
+++ text20150803/src/text06.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class text06 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scn=new Scanner(System.in);
+		System.out.print("請輸入三角形的底邊長");
+		String name=scn.next();
+		System.out.print("請輸入三角形的高");
+		String abcd=scn.next();
+		
+	}
+
+}
