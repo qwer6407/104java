959080de0e7cfdf248ca5c7c5661cc8efd67cadc
qwer6407 <104021021@live.asia.com.tw>
2015-08-18 10:09:14 +0800

    week3day2

---
+++ text20150803/src/Method01.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Method01 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scn=new Scanner(System.in);
+	}
+
+}
